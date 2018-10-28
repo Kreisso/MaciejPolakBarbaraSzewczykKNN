59e02f0d61edc3b5fce4a85707bacbf94fe45764
Barbara <bszewczyk97@gmail.com>
2018-10-28 12:24:35 +0100

    clean code v0.5

--- venv/bin/main.py
+++ venv/bin/main.py
@@ -6,14 +6,13 @@ from KNN import KNN
 iris_test_filename = 'iris.data.test.csv'
 iris_learning_filename = 'iris.data.learning.csv'
 
-irisTest = pd.read_csv(iris_test_filename, sep=',', decimal='.', header=None, names= ['sepal_length', 'sepal_width', 'petal_length', 'petal_width','target'])
-irisLearning = pd.read_csv(iris_learning_filename, sep=',', decimal='.', header=None, names= ['sepal_length', 'sepal_width', 'petal_length', 'petal_width','target'])
-unTagList = irisTest.iloc[:, :-1].values
-testTargets = irisTest.iloc[:,4].values
+irisTest = pd.read_csv(iris_test_filename, sep=',', decimal='.', header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'target'])
+irisLearning = pd.read_csv(iris_learning_filename, sep=',', decimal='.', header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'target'])
+untagged_test_data = irisTest.iloc[:, :-1].values
+test_data_targets = irisTest.iloc[:, 4].values
 k = 3
 
-K1 = KNN(k,irisLearning)
-
-score = K1.score(unTagList, testTargets)
+K1 = KNN(k, irisLearning)
+score = K1.score(untagged_test_data, test_data_targets)
 
 print(score)
