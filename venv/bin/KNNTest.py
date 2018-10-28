4fa6854de4dacb4803079d139f461448ae9ebf76
Barbara <bszewczyk97@gmail.com>
2018-10-28 17:11:07 +0100

    Added new data to unittest Added printing result of the algorithm

--- venv/bin/KNNTest.py
+++ venv/bin/KNNTest.py
@@ -1,23 +1,24 @@
 import unittest
 from KNN import KNN
-import main
 import pandas as pd
 import numpy as np
 
 class KNNTest(unittest.TestCase):
-    iris_test_filename = 'iris.data.test.csv'
-    iris_learning_filename = 'iris.data.learning.csv'
+    unittest_test_filename = 'unittest.data.test.csv'
+    unittest_learning_filename = 'unittest.data.learning.csv'
 
-    irisTest = pd.read_csv(iris_test_filename, sep=',', decimal='.', header=None,
+    unittest_test_data = pd.read_csv(unittest_test_filename, sep=',', decimal='.', header=None,
                            names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'target'])
-    irisLearning = pd.read_csv(iris_learning_filename, sep=',', decimal='.', header=None,
+    unittest_learning_data = pd.read_csv(unittest_learning_filename, sep=',', decimal='.', header=None,
                                names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'target'])
-    untagged_test_data = irisTest.iloc[:, :-1].values
-    test_data_targets = irisTest.iloc[:, 4].values
+    untagged_test_data = unittest_test_data.iloc[:, :-1].values
+    test_data_targets = unittest_test_data.iloc[:, 4].values
 
     def test_score(self):
-        K2 = KNN(3, self.irisLearning)
-        self.assertEquals(93.33333333333333, K2.score(self.untagged_test_data, self.test_data_targets))
+        K2 = KNN(5, self.unittest_learning_data)
+        print(K2.score(self.untagged_test_data, self.test_data_targets))
+        print(K2.predict(self.untagged_test_data))
+        self.assertEqual(75, K2.score(self.untagged_test_data, self.test_data_targets))
 
     if __name__ == '__main__':
         unittest.main()
